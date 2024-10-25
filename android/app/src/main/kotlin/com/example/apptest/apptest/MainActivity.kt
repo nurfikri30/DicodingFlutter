de252b8646a7272256a9a5f7f1d0cc93234bf209
nurfikri30 <muhammadnurfikri86@gmail.com>
2024-10-25 03:49:24 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/apptest/apptest/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.apptest.apptest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:apptest/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:apptest/detail_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    title: 'Wisata Bandung',
+    theme: ThemeData(),
+    home: const MainScreen(),
+    );
+  }
+
+}
